86b12cb972d5f50b3ee7352e50055acd5a86120b
jeffrey.miller445 <jeffrey.miller445@gmail.com>
2018-11-24 08:04:23 -0500

    update1-23Nov18

--- assets/javascript/triva.js
+++ assets/javascript/triva.js
@@ -1,11 +1,17 @@
 // game.js
 
+
+
 var wins;
 var losses;
 var unanswerd;
 // var time = 30;
 
-var number = 30;
+var timer = 60;
+var number = timer;
+
+var choice = 0;
+
 var isRunning = false;
 
     //  Variable that will hold our interval ID when we execute
@@ -13,7 +19,7 @@ var isRunning = false;
 var intervalId;
 
 var ques = 0;
-
+var qNum = 0;
 
 var trivaQ = { 
         arrayOfQ: [
@@ -58,31 +64,31 @@ var trivaQ = {
     // ********************  function definitions  ********************************
 
     function getQuestion(q) {
-      i = q;
-      console.log("qNum is type= " + typeof i);
-      console.log("qNum = " + i);
-        
-        
-        $("#show-question").html("<h3>" + trivaQ.arrayOfQ[i].qText + "</h3>");
+      
+        qNum = q; 
+    
+        $("#show-question").html("<h3>" + trivaQ.arrayOfQ[qNum].qText + "</h3>");
         for (mc = 0; mc < 4; mc++){
             
         // console.log("choices= " + trivaQ.arrayOfQ[i].multipleChoices[mc])
 
-        $("#show-choices" + mc).html("<h3 class='choices' data=" + mc + ">" + trivaQ.arrayOfQ[i].multipleChoices[mc] + "</h3>");
+        $("#show-choices" + mc).html("<button class='button' id='choice' type='button' value=" + mc + ">" + trivaQ.arrayOfQ[qNum].multipleChoices[mc] + "</button>");
+        // $("#show-choices" + mc).html("<h3 class='choices' data=" + mc + ">" + trivaQ.arrayOfQ[qNum].multipleChoices[mc] + "</h3>");
         // $(".choices").html("<h3>" + trivaQ.arrayOfQ[i].multipleChoices[mc] + "</h3>");
         };
       };
 
     function getAnswer(q){
-        i = q;
-        console.log("question number in getAnswer= " + i);
-        $("#show-answer").html("<h4>" + trivaQ.arrayOfQ[i].multipleChoices[trivaQ.arrayOfQ[i].aText] + "</h4>");
-        // $("#show-status").show();
-        // $("#show-answer").show();
-        // $("#").html("<h4>" + trivaQ.arrayOfQ[i].multipleChoices[trivaQ.arrayOfQ[i].aText] + "</h4>")
+      
+        qNum = q;
+        
+        $("#questionAndChoices").hide();
+        $("#show-answer").html("<h4>" + trivaQ.arrayOfQ[qNum].multipleChoices[trivaQ.arrayOfQ[qNum].aText] + "</h4>");
+        
       };
 
     function run() {
+      // alert("in run");
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
         isRunning = true;
@@ -119,81 +125,137 @@ var trivaQ = {
   
       //  The stop function
     function stop() {
-  
+
         //  Clears our intervalId
         //  We just pass the name of the interval
         //  to the clearInterval function.
+
         clearInterval(intervalId);
-        $("#questionAndChoices").hide()
-        $("#show-status").html("<h4>Out of Time!</h4><h4>The Correct Answer was:</h4>");
-        getAnswer(ques);
-        isRunning = false;
-        console.log("isRunning in stop= " + isRunning);
-
       }
-  
 
+    // *********************************************************  
 
+    function startGame() {
 
-
-// *********************************************************  
-function startGame() {
-    // alert("Game has started");
+      $(document).ready(function() {
     
-    $("#start-button").hide();
+      $("#restart").hide();
+      $("#start-button").hide();
+      // $("#questionAndChoices").show();
+      console.log("question number in startGame= " + ques);
+      run();
+
+      getQuestion(ques);
     
-    run();
-    // isRunning=true;
-
-    // do {
-    getQuestion(ques);
-    playGame(ques);
-    // } while ( isRunning);
-
-  //   do {
-  //     isRunning = true;
-      
-  //     getQuestion(ques);
-
-  // } while ( isRunning );
-
-    // run();
-    // decrement();
-    // getQuestion(ques);
-};
+      playGame(ques);
+      });
+    }
 
 function playGame(q) {
-  i = q;
-  console.log("isRunning in playGame= " + isRunning);
-  $(document).on("click", ".choices", function() {
-    dataChoice = parseInt($(this).attr('data'));
 
-    if ( dataChoice === trivaQ.arrayOfQ[i].aText){
-      alert("You have a correct answer");
-      nextq = i + 1;
-      getQuestion(nextq);
-    }
-    else {
+  qNum = q;
+
+  $("#show-status").hide();
+  $("#show-answer").hide();
+  $("#questionAndChoices").show();
+  $(".choices").show();
+
+  // console.log("isRunning in playGame= " + isRunning);
+
+  // $(".choices").on("click", function() {
+    $(".button").on("click", function() {
+    // dataChoice = parseInt($(this).attr('data'));
+    dataChoice = parseInt($(this).attr('value'));
+
+    console.log("data= " + dataChoice);
+    console.log("aText= " + trivaQ.arrayOfQ[qNum].aText);
+   
+  // $(document).on("click", ".choices", function() {})
+  
+    if ( dataChoice === trivaQ.arrayOfQ[qNum].aText){
+      // alert("You have a correct answer");
+
+      $("#show-status").show();
+      $("#show-answer").show(); 
       
+      stop();
+      number = timer;
+      
+      $("#show-status").html("<h4>Correct</h4><h4>The Answer was:</h4>");
+
+      getAnswer(qNum);
+
+      // ques +=1;
+      ques++;
+
+      if (ques < trivaQ.arrayOfQ.length) {
+
+        // $("#show-question", "#show-choices0", 
+        //   "#show-choices1", "#show-choices2",
+        //   "#show-choices3").empty();
+
+        // setTimeout(console.log("we are in timeout for 5 sec"), 1000 * 5);
+          setTimeout(startGame,3000);
+
+         }
+         else {
+
+          //  display restart game button
+          ques = 0;
+          qNum = 0;
+
+          console.log("ques1= " + ques);
+          console.log("qnum1= " + qNum);
+
+          $("#restart").show()
+
+         }
+      }
+
+    else {
+
+      console.log("wrong answer");
+
       $("#questionAndChoices").hide();
+
+      stop();
+
+      $("#show-status").show();
+      $("#show-answer").show();
+      
       $("#show-status").html("<h4>Wrong Answer</h4><h4>The Correct Answer was:</h4>");
 
-      getAnswer(i);
+      getAnswer(qNum);
+
+       // next question
+      //  ques += 1; 
+      ques++;
+
+      if (ques < trivaQ.arrayOfQ.length) {
+        
+        // setTimeout(console.log("we are in timeout for 5 sec"), 1000 * 5);
+          setTimeout(startGame,3000);
+
+         }
+         else {
+
+          //  display restart game button
+          ques = 0;
+          qNum = 0;
+
+          console.log("ques2= " + ques);
+          console.log("qnum2= " + qNum);
+
+          $("#restart").show()
+
+         }
+      }
   
-      $("#show-answer").hide();
-      $("#show-status").hide();
       
-      alert("builtin wait before next question");
-      nextq = i + 1;
-      getQuestion(nextq);
-      $("#questionAndChoices").show();
+      // ques +=1;
+      // setTimeout(startGame,3000);
       
-    }
-  console.log("answer selected= " + typeof dataChoice + " correct answer= " + typeof trivaQ.arrayOfQ[i].aText);
-  console.log("the correct answer is= " + trivaQ.arrayOfQ[i].multipleChoices[trivaQ.arrayOfQ[i].aText]);
-  });
+    
+}); 
 
 };
-
-
-
